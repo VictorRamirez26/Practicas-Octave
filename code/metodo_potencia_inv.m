6d82908ec4f7de8f0d622bd55e254836dd0cf4cc
Victor Ramirez <victorriver6@hotmail.com>
2024-11-26 19:44:00 -0300

    Metodo potencia inversa

---
+++ code/metodo_potencia_inv.m
@@ -0,0 +1,32 @@
+function metodo_potencia_inv
+  clc
+  A = [-10, 4 ; -4, 0];
+  A_inv = inv(A);
+  x = [2;2];
+  error = 0;
+  tol = 0.00001;
+  alpha = [0;0];
+  k = 0;
+
+  while error < 1 - tol
+
+    x_norm = (1/(norm(x,inf))) * x;
+
+    xk = A_inv * x_norm;
+
+    xk_norm = xk / (norm(xk,inf)); %Autovector
+
+    error = abs((transpose(xk_norm) * xk_norm)/(transpose(x_norm) * x_norm));
+
+    alpha = xk ./ x_norm;
+    x = xk;
+    k = k + 1;
+  endwhile
+
+  k
+  xk_norm
+  lambda = 1/alpha(1)
+
+  izq = A_inv * xk %Lado izquierdo de la ecuación
+  der = (1 / lambda) * xk %Lado derecho de la ecuación
+end
